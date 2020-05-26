322774d9b72db1fb404ebdccfa20c9b92a8317dc
VitoMurillo <A.Murillo@ofdataanalytics.com>
2020-05-26 15:05:46 +0200

    Documentation Endpoint Implemented
    Doc Endpoint implemented via flask_swagger_ui

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required, current_identity
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
@@ -40,10 +40,27 @@ class report(Resource):
     def post(self):
         return {'POST Request':'Hello World'}
 
+class documentation_endpoint(Resource):
+    def get(path):
+        return send_from_directory('static', path)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name':'SBTi-API'
+    }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
 
 api.add_resource(temp_score, '/temperature_score/')
 api.add_resource(portfolio_coverage, '/portfolio_coverage/')
 api.add_resource(report, '/report/')
+api.add_resource(documentation_endpoint, '/static/<path:path>')
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
