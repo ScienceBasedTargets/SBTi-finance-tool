81b32e6071e47bcf4d4f8f637e59559fab6940f8
OFJanPostema <j.postema@ofdataanalytics.com>
2020-05-26 11:12:20 +0200

    #2 Add temp score and emission to the reporting

--- SBTi/reporting.py
+++ SBTi/reporting.py
@@ -8,7 +8,7 @@ class Reporting:
     This class provides a report on a portfolio.
     """
 
-    def __init__(self, fallback_emissions: float = 3.2, fallback_score: float = -1.0):
+    def __init__(self, fallback_emissions: float = -1.0, fallback_score: float = 3.2):
         """
         Initialize the class.
         :param fallback_emissions: The emission if a company is not found
@@ -16,6 +16,20 @@ class Reporting:
         """
         self.fallback_score = fallback_score
         self.fallback_emissions = fallback_emissions
+
+    def get_temp_score(self, data_provider: DataProvider, company: str) -> float:
+        try:
+            emissions, temp_score = data_provider.get_report(company)
+            return temp_score
+        except CompanyNotFoundException as e:
+            return self.fallback_score
+
+    def get_emissions(self, data_provider: DataProvider, company: str) -> float:
+        try:
+            emissions, temp_score = data_provider.get_report(company)
+            return emissions
+        except CompanyNotFoundException as e:
+            return self.fallback_emissions
 
     def get_report(self, data_provider: DataProvider, portfolio: List[dict]) -> Tuple[List[dict], float, float, float]:
         """
@@ -31,8 +45,8 @@ class Reporting:
                     data_provider.get_report(company["id"])
                 coverage += 1
             except CompanyNotFoundException:
-                portfolio[i_company]["temp_score"] = self.fallback_score
                 portfolio[i_company]["emissions"] = self.fallback_emissions
+                portfolio[i_company]["temp_score"] = self.fallback_score
 
         total = sum([company["proportion"] for company in portfolio])
         if total > 0:
