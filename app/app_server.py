58b56f1425763434f0e73741c2350b2a07ff04da
OFJanPostema <j.postema@ofdataanalytics.com>
2020-05-28 10:07:46 +0200

    #13 Generated licenses

--- app/app_server.py
+++ app/app_server.py
@@ -3,38 +3,41 @@ from flask_restful import Resource, Api, reqparse
 from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+app.config['PROPAGATE_EXCEPTIONS'] = True  # To allow flask propagating exception even if debug is set to false on app
 api = Api(app)
+
 
 class temp_score(Resource):
 
     def get(self):
-        return {'GET Request':'Hello World'}
+        return {'GET Request': 'Hello World'}
 
     def post(self):
-        return {'POST Request':'Hello World'}
+        return {'POST Request': 'Hello World'}
 
 
 class portfolio_coverage(Resource):
 
     def get(self):
-        return {'GET Request':'Hello World'}
+        return {'GET Request': 'Hello World'}
 
     def post(self):
-        return {'POST Request':'Hello World'}
+        return {'POST Request': 'Hello World'}
 
 
 class report(Resource):
 
     def get(self):
-        return {'GET Request':'Hello World'}
+        return {'GET Request': 'Hello World'}
 
     def post(self):
-        return {'POST Request':'Hello World'}
+        return {'POST Request': 'Hello World'}
+
 
 class documentation_endpoint(Resource):
     def get(path):
         return send_from_directory('static', path)
+
 
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
@@ -42,12 +45,11 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
     config={
-        'app_name':'SBTi-API'
+        'app_name': 'SBTi-API'
     }
 )
 
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-
 
 api.add_resource(temp_score, '/temperature_score/')
 api.add_resource(portfolio_coverage, '/portfolio_coverage/')
