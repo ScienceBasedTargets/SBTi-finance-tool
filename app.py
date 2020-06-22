4224348a1600f853f8fc3ff4f67804282e74a5b8
VitoMurillo <A.Murillo@ofdataanalytics.com>
2020-06-22 16:17:59 +0200

    Additional Endpoint and HTTP Request
    Allows the client to import or replace portfolio

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, request, send_from_directory
 from flask_restful import Resource, Api, reqparse
 from flask_swagger_ui import get_swaggerui_blueprint
+import os
+from flask_uploads import UploadSet, configure_uploads, ALL
 
+
+PATH = 'C:/Projects/SBTi/documents'
 app = Flask(__name__)
-app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+files = UploadSet('files',ALL)
+# app.config['PROPAGATE_EXCEPTIONS'] = True # To allow flask propagating exception even if debug is set to false on app
+app.config['UPLOADS_DEFAULT_DEST'] = PATH
+configure_uploads(app,files)
 api = Api(app)
+
 
 class temp_score(Resource):
 
-    # parameters
     # parser = reqparse.RequestParser()
-    # parser.add_argument('price',
-    #     type=float,
-    #     required=True,
-    #     help="This field cannot be left blank!"
-    # )
+    # parser.add_argument('price', type=float, required=True, help="This field cannot be left blank!")
 
     def get(self):
         return {'GET Request':'Hello World'}
@@ -29,7 +32,9 @@ class portfolio_coverage(Resource):
         return {'GET Request':'Hello World'}
 
     def post(self):
-        return {'POST Request':'Hello World'}
+        return{'POST Request': 'File Save'}
+
+
 
 
 class report(Resource):
@@ -40,9 +45,29 @@ class report(Resource):
     def post(self):
         return {'POST Request':'Hello World'}
 
+
 class documentation_endpoint(Resource):
     def get(path):
         return send_from_directory('static', path)
+
+
+class import_portfolio(Resource):
+
+    def get(self):
+        return {'GET Request':'Hello World'}
+
+    def post(self):
+        files.save(request.files['file'])
+        return{'POST Request': 'File Save'}
+
+    def put(self):
+        files.save(request.files['file'])
+        remove_doc = request.args.get('document_replace')
+        for root, dirs, file in os.walk(PATH):
+            for f in file:
+                if remove_doc == f.split('.')[0]:
+                    os.remove(os.path.join(root, f))
+        return{'PUT Request':'Document Replaced'}
 
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
@@ -55,12 +80,12 @@ swaggerui_blueprint = get_swaggerui_blueprint(
 )
 
 app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-
-
-api.add_resource(temp_score, '/temperature_score/')
-api.add_resource(portfolio_coverage, '/portfolio_coverage/')
-api.add_resource(report, '/report/')
+api.add_resource(temp_score, '/temperature_score')
+api.add_resource(portfolio_coverage, '/portfolio_coverage')
+api.add_resource(report, '/report')
 api.add_resource(documentation_endpoint, '/static/<path:path>')
+api.add_resource(import_portfolio, '/import_portfolio')
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
+
