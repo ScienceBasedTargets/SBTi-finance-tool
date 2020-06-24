11ec0cbe88363ee886f005ddefe31d73bfacba73
OFJanPostema <j.postema@ofdataanalytics.com>
2020-06-24 11:58:41 +0200

    #35 Update the abstract class to include documentation on the required columns

--- SBTi/data/data_provider.py
+++ SBTi/data/data_provider.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Tuple
+import pandas as pd
 
 
 class DataProvider(ABC):
@@ -7,19 +7,65 @@ class DataProvider(ABC):
     General data provider super class.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, companies: list):
+        """
+        Create a new data provider instance.
+
+        :param companies: A list of companies. Each company should be a dict with a "company_name" and "company_id"
+                            field.
+        """
+        self.companies = companies
 
     @abstractmethod
-    def get_report(self, company) -> Tuple[float, float]:
+    def get_targets(self) -> pd.DataFrame:
         """
-        Get the emissions and the temperature score given a company. Throws a CompanyNotFoundException if the company
-        is not found.
+        Get all the targets for the whole portfolio of companies. This should return a dataframe, containing at least
+        the following columns:
+
+        * company_name: The name of the company
+        * company_id: The ID of the company
+        * target_reference_number: Int *x* of Abs *x*
+        * target_reference_number: Int *x* of Abs *x*
+        * scope: The scope of the target. This should be a valid scope in the SR15 mapping
+        * base_year: The base year of the target
+        * start_year: The start year of the target
+        * target_year: The year when the target should be achieved
+        * reduction_from_base_year: Targeted reduction in emissions from the base year
+        * emissions_in_scope: Company emissions in the target's scope at start of the base year
+        * achieved_reduction: The emission reduction that has already been achieved
+
+        :return: A dataframe containing the targets
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_company_data(self) -> pd.DataFrame:
+        """
+        Get all relevant data for a certain company. Should return a dataframe, containing at least the following
+        columns:
+
+        * company_name: The name of the company
+        * company_id: The ID of the company
+        * industry: The industry the company is working in. This should be a valid industry in the SR15 mapping. If not
+            it will be converted to "Others" (or whichever value is set in the config as the default
+        * s1s2_emissions: Total company emissions in the S1 + S2 scope
+        * s3_emissions: Total company emissions in the S3 scope
+        * portfolio_weight: The weight of the company in the portfolio. Only required to use the WATS portfolio
+            aggregation.
+        * market_cap: Market capitalization of the company. Only required to use the MOTS portfolio aggregation.
+        * investment_value: The investment value of the investment in this company. Only required to use the MOTS, EOTS,
+            ECOTS and AOTS portfolio aggregation.
+        * company_enterprise_value: The enterprise value of the company. Only required to use the EOTS portfolio
+            aggregation.
+        * company_ev_plus_cash: The enterprise value of the company plus cash. Only required to use the ECOTS portfolio
+            aggregation.
+        * company_total_assets: The total assets of the company. Only required to use the AOTS portfolio aggregation.
+
 
         :param company: str: The identifier of the company to get the emissions for
-        :return: float, float: The emissions and the temperature score
+        :return: A dataframe containing the company data
         """
-        pass
+        raise NotImplementedError
 
 
 class CompanyNotFoundException(Exception):
