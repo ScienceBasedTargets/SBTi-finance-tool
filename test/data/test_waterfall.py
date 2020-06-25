c225f664ba0f532789662e09c955cf8e4029c4dd
OFJanPostema <j.postema@ofdataanalytics.com>
2020-06-25 15:39:11 +0200

    #37 Add the waterfall method to handle multiple data providers

---
+++ test/data/test_waterfall.py
@@ -0,0 +1,49 @@
+import SBTi.data
+from SBTi.data.csv import CSVProvider
+
+import os
+import unittest
+
+
+class TestWaterfall(unittest.TestCase):
+    """
+    Test the waterfall method for loading data from the data providers.
+    """
+
+    def setUp(self) -> None:
+        """
+        Create the providers and list of companies which we'll use later on.
+        """
+        self.companies = [
+            {"company_name": "Company AA", "company_id": None},
+            {"company_name": "Company F", "company_id": None},
+            {"company_name": "Company Q", "company_id": None},
+            {"company_name": "Company N", "company_id": None},
+        ]
+        self.data_providers = [
+            CSVProvider({
+                "path": os.path.join(os.path.dirname(os.path.realpath(__file__)), "../", "inputs",
+                                     "data_test_waterfall_a.csv")
+            }),
+            CSVProvider({
+                "path": os.path.join(os.path.dirname(os.path.realpath(__file__)), "../", "inputs",
+                                     "data_test_waterfall_b.csv")
+            }),
+            CSVProvider({
+                "path": os.path.join(os.path.dirname(os.path.realpath(__file__)), "../", "inputs",
+                                     "data_test_waterfall_c.csv")
+            }),
+        ]
+
+    def test_company_data(self) -> None:
+        """
+        Test whether data is retrieved as expected.
+        """
+        company_data = SBTi.data.get_company_data(self.data_providers, self.companies)
+        assert len(company_data) == 4, "The numbers of companies does not match"
+
+
+if __name__ == "__main__":
+    test = TestWaterfall()
+    test.setUp()
+    test.test_company_data()
