b47693e0d13d63862cd016a557ec44b0169c1161
MikeM <Mike.Markus@Ortec-Finance.com>
2020-07-21 11:55:43 +0200

    Fix console warning in spec

--- app/static/frontend/src/app/app.component.spec.ts
+++ app/static/frontend/src/app/app.component.spec.ts
@@ -2,13 +2,15 @@ import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
+        FormsModule
       ],
       declarations: [
         AppComponent
