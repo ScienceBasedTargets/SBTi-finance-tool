a911da34e16e2b3403c6a21df88e56b15fa395a1
OFJanPostema <j.postema@ofdataanalytics.com>
2020-07-29 14:48:31 +0200

    #159 Cleanup newline

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     author_email='sbti@ortec-finance.com',
     packages=find_packages(),
     package_data={
-
         'SBTi': ['inputs/sr15_mapping.xlsx', 'inputs/regression_model_summary.xlsx',
                  'inputs/current-Companies-Taking-Action-191.xlsx'],
     },
