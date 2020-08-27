c9d5f919f00f912db56e48814f95e2d741cc774c
OFJanPostema <j.postema@ofdataanalytics.com>
2020-08-27 10:57:12 +0200

    #178 Add error message when no data provider is found

--- SBTi/utils.py
+++ SBTi/utils.py
@@ -25,30 +25,34 @@ DATA_PROVIDER_MAP: Dict[str, Type[data.DataProvider]] = {
 }
 
 
-def get_data_providers(data_providers_config: List[dict], data_providers_input: List[str]) -> List[data.DataProvider]:
+def get_data_providers(data_providers_configs: List[dict], data_providers_input: List[str]) -> List[data.DataProvider]:
     """
     Determines which data provider and in which order should be used.
 
-    :param data_providers_config: A list of data provider configurations
+    :param data_providers_configs: A list of data provider configurations
     :param data_providers_input: A list of data provider names
     :return: a list of data providers in order.
     """
+    logger = logging.getLogger(__name__)
     data_providers = []
-    for data_provider_config in data_providers_config:
+    for data_provider_config in data_providers_configs:
         data_provider_config["class"] = DATA_PROVIDER_MAP[data_provider_config["type"]](**data_provider_config["parameters"])
         data_providers.append(data_provider_config)
 
     selected_data_providers = []
     for data_provider_name in data_providers_input:
+        found = False
         for data_provider_config in data_providers:
             if data_provider_config["name"] == data_provider_name:
                 selected_data_providers.append(data_provider_config["class"])
+                found = True
                 break
+        if not found:
+            logger.warning("The following data provider could not be found: {}".format(data_provider_name))
 
-    # TODO: When the user did give us data providers, but we can't match them this fails silently, maybe we should
-    # fail louder
     if len(selected_data_providers) == 0:
-        selected_data_providers = [data_provider_config["class"] for data_provider_config in data_providers]
+        raise ValueError("None of the selected data providers are available. The following data providers are valid "
+                         "options: " + ", ".join(data_provider["name"] for data_provider in data_providers_configs))
     return selected_data_providers
 
 
