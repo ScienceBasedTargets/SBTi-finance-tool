9c848a59e24814a425c2011965962f484f0d7a03
Christian Schmidli <christian.schmidli@king.com>
2020-09-11 13:05:58 +0200

    adding some more tests & cleanup

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -8,7 +8,7 @@ from SBTi.interfaces import (
     PortfolioCompany,
 )
 from SBTi.target_validation import TargetProtocol
-from SBTi.temperature_score import TemperatureScore
+from SBTi.temperature_score import EngagementType, Scenario, ScenarioType, TemperatureScore
 from SBTi.data.data_provider import DataProvider
 from SBTi.portfolio_aggregation import PortfolioAggregationMethod
 import copy
@@ -24,12 +24,6 @@ class TestDataProvider(DataProvider):
         self.targets = targets
         self.companies = companies
 
-    # def set_targets(self, targets: List[IDataProviderTarget]):
-    #     self.targets = targets
-
-    # def set_company_data(self, companies: List[IDataProviderCompany]):
-    #     self.companies = companies
-
     def get_sbti_targets(self, companies: list) -> list:
         return []
 
@@ -40,7 +34,14 @@ class TestDataProvider(DataProvider):
         return self.companies
 
 
-class CoreTest(unittest.TestCase):
+class EndToEndTest(unittest.TestCase):
+    """
+    This class is containing a set of end to end tests:
+    - basic flow from creating companies/targets up to calculating aggregated values
+    - edge cases for scenarios and grouping
+    - high load tests (>1000 targets)
+    - testing of all different input values and running thru the whole process (tbd)
+    """
     def setUp(self):
         company_id = "BaseCompany"
         self.company_base = IDataProviderCompany(
@@ -79,6 +80,9 @@ class CoreTest(unittest.TestCase):
         )
 
     def test_basic(self):
+        """
+        This test is just a very basic workflow going thru all calculations up to temp score
+        """
 
         # Setup test provider
         company = copy.deepcopy(self.company_base)
@@ -101,46 +105,26 @@ class CoreTest(unittest.TestCase):
         self.assertIsNotNone(scores)
         self.assertEqual(len(scores.index), 3)
 
-        assert True
-
     def test_chaos(self):
         # TODO: go thru lots of different parameters on company & target level and try to break it
         pass
 
+
+   
+
     def test_basic_flow(self):
+        """
+        This test is going all the way to the aggregated calculations
+        """
 
-        # Set up 2 companies based on the base company
-        company_ids = ["A", "B"]
-        companies: List[IDataProviderCompany] = []
-        targets: List[IDataProviderTarget] = []
-        pf_companies: List[PortfolioCompany] = []
-        for company_id in company_ids:
-
-            # company
-            company = copy.deepcopy(self.company_base)
-            company.company_id = company_id
-            companies.append(company)
-
-            # target
-            target = copy.deepcopy(self.target_base)
-            target.company_id = company_id
-            targets.append(target)
-
-            # pf company
-            pf_company = PortfolioCompany(
-                company_name=company_id,
-                company_id=company_id,
-                investment_value=100,
-                company_isin=company_id,
-            )
-            pf_companies.append(pf_company)
+        companies, targets, pf_companies = self.create_base_companies(["A", "B"])
 
         data_provider = TestDataProvider(companies=companies, targets=targets)
 
         # Calculate scores & Aggregated values
         temp_score = TemperatureScore(
             time_frames=[ETimeFrames.MID],
-            scopes=[EScope.S1S2],
+            scopes=[ EScope.S1S2, EScope.S1S2S3],
             aggregation_method=PortfolioAggregationMethod.WATS,
         )
 
@@ -149,13 +133,13 @@ class CoreTest(unittest.TestCase):
         agg_scores = temp_score.aggregate_scores(scores)
 
         # verify that results exist
-        self.assertEqual(agg_scores.mid.S1S2.all.score, 0.54)
+        self.assertEqual(agg_scores.mid.S1S2.all.score, 0.43)
 
     # Run some regression tests
     @unittest.skip("only run for longer test runs")
     def test_regression_companies(self):
 
-        nr_companies = 100
+        nr_companies = 1000
 
         # test 10000 companies
         companies: List[IDataProviderCompany] = []
@@ -198,10 +182,107 @@ class CoreTest(unittest.TestCase):
         scores = temp_score.calculate(portfolio_data)
         agg_scores = temp_score.aggregate_scores(scores)
 
-        self.assertAlmostEqual(agg_scores.mid.S1S2.all.score, 0.54)
+        self.assertAlmostEqual(agg_scores.mid.S1S2.all.score, 0.43)
 
+
+    def test_grouping(self):
+        """
+        Testing the grouping feature with two different industry levels and making sure the results are present
+        """
+        # make 2+ companies and group them together
+        industry_levels = ["Manufacturer", "Energy"]
+        company_ids = ["A", "B"]
+        companies_all: List[IDataProviderCompany] = []
+        targets_all: List[IDataProviderTarget] = []
+        pf_companies_all: List[PortfolioCompany] = []
+
+        for ind_level in industry_levels:
+
+            company_ids_with_level = [f"{ind_level}_{company_id}" for company_id in company_ids]
+
+            companies, targets, pf_companies = self.create_base_companies(company_ids_with_level)
+            for company in companies:
+                company.industry_level_1 = ind_level
+            
+            companies_all.extend(companies)
+            targets_all.extend(targets)
+            pf_companies_all.extend(pf_companies)
+
+            
+
+        data_provider = TestDataProvider(companies=companies_all, targets=targets_all)
+
+        temp_score = TemperatureScore(
+                time_frames=[ETimeFrames.MID],
+                scopes=[EScope.S1S2],
+                aggregation_method=PortfolioAggregationMethod.WATS,
+                grouping=["industry_level_1"]
+            )
+
+        portfolio_data = SBTi.utils.get_data([data_provider], pf_companies_all)
+        scores = temp_score.calculate(portfolio_data)
+        agg_scores = temp_score.aggregate_scores(scores)
+
+        for ind_level in industry_levels:
+            self.assertAlmostEqual(agg_scores.mid.S1S2.grouped[ind_level].score, 0.43)
+
+
+    def test_score_cap(self):
+        
+        companies, targets, pf_companies = self.create_base_companies(["A"])
+        data_provider = TestDataProvider(companies=companies, targets=targets)
+
+        # add a Scenario that will trigger the score cap function
+        scenario = Scenario()
+        scenario.engagement_type = EngagementType.SET_TARGETS
+        scenario.scenario_type = ScenarioType.APPROVED_TARGETS
+
+        temp_score = TemperatureScore(
+                time_frames=[ETimeFrames.MID],
+                scopes=[EScope.S1S2],
+                aggregation_method=PortfolioAggregationMethod.WATS,
+                scenario=scenario
+            )
+
+        portfolio_data = SBTi.utils.get_data([data_provider], pf_companies)
+        scores = temp_score.calculate(portfolio_data)
+        agg_scores = temp_score.aggregate_scores(scores)
+
+        # add verification
+
+
+    def create_base_companies(self,company_ids:List[str]):
+        """
+        This is a helper method to create base companies that can be used for the test cases
+        """
+        companies: List[IDataProviderCompany] = []
+        targets: List[IDataProviderTarget] = []
+        pf_companies: List[PortfolioCompany] = []
+        for company_id in company_ids:
+
+            # company
+            company = copy.deepcopy(self.company_base)
+            company.company_id = company_id
+            companies.append(company)
+
+            # target
+            target = copy.deepcopy(self.target_base)
+            target.company_id = company_id
+            targets.append(target)
+
+            # pf company
+            pf_company = PortfolioCompany(
+                company_name=company_id,
+                company_id=company_id,
+                investment_value=100,
+                company_isin=company_id,
+            )
+            pf_companies.append(pf_company)
+
+        return companies, targets, pf_companies
 
 if __name__ == "__main__":
-    test = CoreTest()
+    test = EndToEndTest()
     test.setUp()
     test.test_basic()
+    test.test_basic_flow()
