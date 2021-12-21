45f041bd0eb5ab70c234635f606121b787793519
Christian Schmidli <christian.schmidli@gmail.com>
2021-12-21 16:28:49 +0100

    add autoapi dependency

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'SBTi Temperature Alignment tool'
-copyright = '2020, Science Based Targets'
-author = 'Science Based Targets'
+project = "SBTi Temperature Alignment tool"
+copyright = "2020, Science Based Targets"
+author = "Science Based Targets"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,15 +27,15 @@ author = 'Science Based Targets'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['autoapi.extension']
+extensions = ["autoapi.extension"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -43,27 +43,28 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
+    "css_files": [
+        "_static/theme_overrides.css",  # override wide tables in RTD theme
     ],
 }
 
 # -- Auto API
-autoapi_dirs = ['../SBTi']
-autoapi_type = 'python'
-autoapi_template_dir = '_autoapi_templates'
+extensions.append("autoapi.extension")
+autoapi_dirs = ["../SBTi"]
+autoapi_type = "python"
+autoapi_template_dir = "_autoapi_templates"
