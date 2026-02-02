1696499ada0c1b74d065521b915472850ac6e425
hubell <58438231+hubell@users.noreply.github.com>
2026-02-02 20:46:42 +0100

    Update base test case end year to 2035 and adjust expected score

--- test/test_edge_cases.py
+++ test/test_edge_cases.py
@@ -42,7 +42,7 @@ class TestDataProvider(DataProvider):
 class EdgeCasesTest(unittest.TestCase):
     def setUp(self):
         company_id = "BaseCompany"
-        self.BASE_COMP_SCORE = 0.43
+        self.BASE_COMP_SCORE = 1.14
         self.company_base = IDataProviderCompany(
             company_name=company_id,
             company_id=company_id,
@@ -68,7 +68,7 @@ class EdgeCasesTest(unittest.TestCase):
             base_year_ghg_s1=100,
             base_year_ghg_s2=0,
             base_year_ghg_s3=0,
-            end_year=2030,
+            end_year=2035,
         )
 
         # pf
